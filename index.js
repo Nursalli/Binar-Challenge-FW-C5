69d695ff7401b57ac0987228a024d764b520ef5d
Muhammad Nursalli <shallimuhammad@gmail.com>
2022-05-23 09:23:04 +0700

    fix3 and Update API From Postman

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 //Use Middleware
 //Built-in Middleware (For Read Public Directory, JSON File and Parsing x-www-urlencoded)
 app.use(express.static('public'));
-app.use(express.json());
+app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded( { extended: false } ));
 
 //Third-Party Middleware (For logger and Layouts)
@@ -101,14 +101,22 @@ app.put('/api/updateUser/:id',
     (req, res) => {
     const errors = validationResult(req);
 
-    if(!errors.isEmpty()){
-        res.status(400).json({ 
-            errors: errors.array() 
-        });
+    const user = findUser(req.params.id);
+    
+    if(user){
+        if(!errors.isEmpty()){
+            res.status(400).json({ 
+                errors: errors.array() 
+            });
+        }else{
+            updateUser(req.body, req.params.id);
+            res.status(200).json({ 
+                success: 'Data User Updated'
+            });
+        }
     }else{
-        updateUser(req.body, req.params.id);
-        res.status(200).json({ 
-            success: 'Data User Updated'
+        res.status(400).json({ 
+            errors: 'User Not Found' 
         });
     }
 });
